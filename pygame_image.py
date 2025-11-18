431c2d988d0efec0d03f1e89088a40191209d338
0c024059bf <c0c24059bf@edu.teu.ac.jp>
2025-11-18 18:01:55 +0900

    kadai2

--- pygame_image.py
+++ pygame_image.py
@@ -29,19 +29,20 @@ def main():
         screen.blit(bg_img, [-x+3200, 0])
 
         key_lst = pg.key.get_pressed() 
+        y=kk_rct.move_ip
         if any(key_lst) == False:
-            kk_rct.move_ip((-1, 0))
+            y((-1, 0))
         if key_lst[pg.K_UP]: 
-            kk_rct.move_ip((0, -1))
+            y((0, -1))
         if key_lst[pg.K_DOWN]: 
-            kk_rct.move_ip((0, +1))
+            y((0, +1))
         if key_lst[pg.K_LEFT]: 
-            kk_rct.move_ip((-2, 0))
+            y((-2, 0))
         if key_lst[pg.K_RIGHT]: 
-            kk_rct.move_ip((1, 0))
+            y((1, 0))
         screen.blit(kk_img, kk_rct)
         pg.display.update()
-        tmr += 1        
+        tmr+=1        
         clock.tick(200)
 
 
