60226ccab9480c66e117d389c6e049f86ee203c4
0c024059bf <c0c24059bf@edu.teu.ac.jp>
2025-11-18 17:23:39 +0900

    練習10

--- pygame_image.py
+++ pygame_image.py
@@ -14,6 +14,9 @@ def main():
     bg2_img = pg.transform.flip(bg2_img, True, False)
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
+    kk_rct = kk_img.get_rect() 
+    kk_rct.center = 300, 200 
+    screen.blit(kk_img, kk_rct)
     tmr = 0
 
     while True:
@@ -24,7 +27,17 @@ def main():
         screen.blit(bg_img, [-x, 0])
         screen.blit(bg_img, [-x+1600, 0])
         screen.blit(bg_img, [-x+3200, 0])
-        screen.blit(kk_img, [300, 200])
+
+        key_lst = pg.key.get_pressed() 
+        if key_lst[pg.K_UP]: 
+            kk_rct.move_ip((0, -1))
+        if key_lst[pg.K_DOWN]: 
+            kk_rct.move_ip((0, +1))
+        if key_lst[pg.K_LEFT]: 
+            kk_rct.move_ip((-1, 0))
+        if key_lst[pg.K_RIGHT]: 
+            kk_rct.move_ip((1, 0))
+        screen.blit(kk_img, kk_rct)
         pg.display.update()
         tmr += 1        
         clock.tick(200)
